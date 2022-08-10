5093306533376d53a5cf406e90b7de5474530300
Wandressa Reis <reiswandressa@gmail.com>
2022-08-09 22:00:14 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const port = 3000;
 
-app.listen(3000)
+app.get('/hello', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(port, function() {
+  console.info('App rodando em http://localhost:' + port)
+});
