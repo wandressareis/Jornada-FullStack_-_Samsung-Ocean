9219b8f6b621561ef4f6681db909609f8372b598
Wandressa Reis <reiswandressa@gmail.com>
2022-08-22 08:54:58 -0400

    atualização do nome do banco de dados

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 
 // const url = "mongodb://localhost:27017";
 const url = "mongodb+srv://admin:Ce8axvEIFEKvscGd@cluster0.oogkm3c.mongodb.net/";
-const dbName = "jornada_fullstack_wandressa";
+const dbName = "Cluster0";
 
 // Declaração da função main()
 async function main() {
