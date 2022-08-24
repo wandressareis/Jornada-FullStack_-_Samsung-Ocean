d6a23bb85f7fd8c6da5192fad96953b7f4a616fd
Wandressa Reis <reiswandressa@gmail.com>
2022-08-24 19:12:55 -0400

    Pulo Implementado

--- frontend/src/components/App/Jogo/Jogo.js
+++ frontend/src/components/App/Jogo/Jogo.js
@@ -2,18 +2,42 @@ import "./Jogo.css";
 import clouds from "../../../assets/clouds.png";
 import mario from "../../../assets/mario.gif";
 import pipe from "../../../assets/pipe.png";
+import React, { useState } from "react";
+
 function Jogo() {
+  console.log("Componente de Jogo Renderizado")
+
+  const [estaPulando, setEstaPulando] = useState(false);
+
+  document.onkeydown = function () {
+    console.log("On Key Down");
+
+    setEstaPulando(true);
+
+    setTimeout(function () {
+      setEstaPulando(false);
+    }, 700);
+  };
+
+
+  let marioClassName = "mario";
+
+  if (estaPulando) {
+    marioClassName = "mario mario-pulo"
+  }
+
+  console.log(15, { estaPulando });
+
   return (
     <div className="jogo">
-      <img className="nuvens" src={clouds} alt="Nuvens"/>
+      <img className="nuvens" src={clouds} alt="Nuvens" />
       
-      <img className="mario" src={mario} alt="Mário"/>
+      <img className={marioClassName} src={mario} alt="Mário" />
       
       <img className="pipe" src={pipe} alt="Pipe"/>
 
       <div className="chao"></div>
     </div>
-
   );
 }
 
