405fffc9dedc0f18845238f0c3734f99d9a2a366
Wandressa Reis <reiswandressa@gmail.com>
2022-08-24 20:39:53 -0400

    Iniciando a implementação do game over

--- frontend/src/components/App/Jogo/Jogo.js
+++ frontend/src/components/App/Jogo/Jogo.js
@@ -2,23 +2,47 @@ import "./Jogo.css";
 import clouds from "../../../assets/clouds.png";
 import mario from "../../../assets/mario.gif";
 import pipe from "../../../assets/pipe.png";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function Jogo() {
   console.log("Componente de Jogo Renderizado")
 
   const [estaPulando, setEstaPulando] = useState(false);
 
+  // Criamos as referências para acessar o css
+  const marioRef = useRef();
+  const pipeRef = useRef();
+
+  function marioEstaNoPipe() {
+    // Acessamos as referÊncias do mario e do cano
+    const mario = marioRef.current;
+    const pipe = pipeRef.current;
+
+    if (!mario || !pipe) {
+      return;
+    }
+    return (
+      pipe.offsetLeft > mario.offsetLeft &&
+      pipe.offsetLeft < mario.offsetLeft + mario.offsetLeft + mario.offsetWidth && 
+      mario.offsetTop + mario.offsetHeight > pipe.offsetTop
+    );
+  }
+
+  setInterval(function () {
+    const valor = marioEstaNoPipe();
+
+    console.log("Mário está no cano?", valor);
+  }, 100);
+
   document.onkeydown = function () {
     console.log("On Key Down");
 
     setEstaPulando(true);
-
+     
     setTimeout(function () {
       setEstaPulando(false);
     }, 700);
   };
-
 
   let marioClassName = "mario";
 
@@ -32,9 +56,9 @@ function Jogo() {
     <div className="jogo">
       <img className="nuvens" src={clouds} alt="Nuvens" />
       
-      <img className={marioClassName} src={mario} alt="Mário" />
+      <img ref={marioRef} className={marioClassName} src={mario} alt="Mário" />
       
-      <img className="pipe" src={pipe} alt="Pipe"/>
+      <img ref={pipeRef} className="pipe" src={pipe} alt="Pipe"/>
 
       <div className="chao"></div>
     </div>
